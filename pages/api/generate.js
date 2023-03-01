5ebaed6c26d5f4e39ed59f88d5a6312768dbae0b
Saswata Ghosh <saswataghoshnit99@gmail.com>
2023-03-01 15:59:03 +0530

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,10 +1,8 @@
 import { Configuration, OpenAIApi } from "openai";
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
@@ -14,7 +12,6 @@ export default async function (req, res) {
     });
     return;
   }
-
   const animal = req.body.animal || '';
   if (animal.trim().length === 0) {
     res.status(400).json({
@@ -24,12 +21,11 @@ export default async function (req, res) {
     });
     return;
   }
-
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
-      temperature: 1,
+      temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -49,7 +45,14 @@ export default async function (req, res) {
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal = animal;
-  return capitalizedAnimal
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `Suggest three names for an animal that is a superhero.
 
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: ${capitalizedAnimal}
+Names:`;
 }
